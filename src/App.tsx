e08403f86fbfa68f29f5cbcc3d3c93944c929e71
cindychang-findingstech <cindy951357@gmail.com>
2025-01-14 11:29:24 +0800

    Fix: don't forget to run npx tailwindcss -i -o

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,41 @@ import moment from "moment";
 
 import Dropdown from "./components/Dropdown.tsx";
 
+const events = [
+  {
+    title: "Event 1",
+    start: new Date(2025, 0, 14, 10, 0), // January 14, 2025, 10:00 AM
+    end: new Date(2025, 0, 14, 12, 0), // January 14, 2025, 12:00 PM
+  },
+  {
+    title: "Event 2",
+    start: new Date(2025, 0, 15, 14, 0), // January 15, 2025, 2:00 PM
+    end: new Date(2025, 0, 15, 15, 30), // January 15, 2025, 3:30 PM
+  },
+];
+
 const localizer = momentLocalizer(moment);
 function App() {
   const { t } = useTranslation();
 
   return (
-    <div className="app-component bg-primary">
-      <h1 className={"text-rose-300"}>{t("webtTitle")}</h1>
-      <Calendar
-        localizer={localizer}
-        events={["aa", "bb"]}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-      />
-      <Dropdown arrowAnimation="rotate" />
+    <div className="app-component bg-primary grid grid-rows-[1fr_6fr_1fr] h-full gap-3">
+      <h1 className="text-rose-300 flex items-center justify-center">
+        {t("webtTitle")}
+      </h1>
+      <div className="h-full">
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          selectable
+          className="h-full border border-gray-300 rounded-lg bg-white p-2"
+        />
+      </div>
+      <div className="flex items-center justify-center">
+        <Dropdown arrowAnimation="rotate" />
+      </div>
     </div>
   );
 }
